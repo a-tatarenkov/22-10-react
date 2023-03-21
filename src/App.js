29fdf9a6714de02e004c6d046cb456c01fcefe5f
Oleksii Tatarenkov <tatarenkov.oleksii@gmail.com>
2023-03-21 10:44:41 +0200

    add test3

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         test for main
         test2 for main
+        test3 for main
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>

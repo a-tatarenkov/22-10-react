14c4cc56edbb5c159308c965c102398b424731ce
Oleksii Tatarenkov <tatarenkov.oleksii@gmail.com>
2023-03-21 10:44:29 +0200

    add test2

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         test for main
+        test2 for main
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
